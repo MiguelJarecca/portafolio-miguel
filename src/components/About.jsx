a26db5378e46d1649f210ac217d40ada13fc2b14
miguel <miguel.jarecca@gmail.com>
2024-01-09 17:24:34 -0500

    se realizo la seccion About

---
+++ src/components/About.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+import imgPerfil from "../img/miguelJC.jpg"
+
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faFacebookF} from '@fortawesome/free-brands-svg-icons';
+import {faLinkedinIn} from '@fortawesome/free-brands-svg-icons';
+import {faGithub} from '@fortawesome/free-brands-svg-icons';
+
+export default function About() {
+  return (
+    <section className='about' id='about'>
+    <div className="about-img">
+      <img src={imgPerfil} alt="" />
+    </div>
+
+    <div className="about-content">
+      <h2 className="heading">Sobre <span>Mí</span></h2>
+      <h3>Hola, Estoy aqui para ayudarte en tu proximo proyecto</h3>
+      <p>Soy Miguel Jarecca Ccallo desarrollador de aplicaciones web con 
+        conocimientos en <span> <strong>back-end</strong> </span> y 
+        <span> <strong>front-end</strong> </span>.
+      </p>
+
+      <div className="social-media">
+        <a href="https://web.facebook.com/miguel.jareccaccallo.1/?locale=es_LA" 
+           target="_blank">
+          <FontAwesomeIcon icon={faFacebookF}/></a>  
+        <a href="https://www.linkedin.com/in/miguel-jarecca-ccallo-2a8226169"
+           target="_blank">
+          <FontAwesomeIcon icon={faLinkedinIn}/></a>  
+        <a href="https://github.com/MiguelJarecca" 
+           target="_blank">
+          <FontAwesomeIcon icon={faGithub}/></a>  
+
+      </div>    
+    </div>
+  </section>
+  )
+}
