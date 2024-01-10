e5fb43720c1fd86592c137f101d0b679bb28da98
miguel <miguel.jarecca@gmail.com>
2024-01-10 16:55:39 -0500

    se trabaja en la seccion contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,25 +1,54 @@
 import React from 'react'
 
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 export default function Contact() {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_x1e3vrd', 'template_na19q4k', form.current, 'u1hxAI5tL07G9vq2X')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
     <section className='contact' id='contact'>
-    <h2 className="heading">Contacta<span>me</span></h2>
 
-    <form action='#'>
-      <div className="input-box">
-          <input type='text' placeholder='Nombre'></input>
-          <input type='email' placeholder='Correo Electronico'></input>
-      </div>
+      <h2 className="heading">Contacta<span> me</span></h2>
 
-      <div className="input-box">
-          <input type='number' placeholder='Numero de Telefono'></input>
-          <input type='email' placeholder='Email Subjet'></input>
-      </div>
+      <form ref={form} onSubmit={sendEmail}>
 
-      <textarea name='' id='' cols="25" rows="7" placeholder='Mensaje'></textarea>
+        <div className="input-box">
+            <input 
+              type='text' 
+              placeholder='Nombre' 
+              name='user_name'>
+            </input>
+            <input 
+              type='email' 
+              placeholder='Correo Electronico' 
+              name='user_email'>
+            </input>
+        </div>
 
-      <input type='submit' value="Enviar Mensaje" className='btn'></input>
-    </form>
+        <textarea 
+          name='message' 
+          cols="24" 
+          rows="7" 
+          placeholder='Mensaje'>
+        </textarea>
+
+        <input type='submit' value="Enviar Mensaje" className='btn'></input>
+
+      </form>
+
     </section>
   )
 }
