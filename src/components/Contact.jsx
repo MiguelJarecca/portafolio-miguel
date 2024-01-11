9c2a44d8b86099b3a558e17feb32cd9ef2fb93c9
miguel <miguel.jarecca@gmail.com>
2024-01-11 12:23:32 -0500

    se hace validaciones en el formulario

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,24 +5,47 @@ import emailjs from '@emailjs/browser';
 
 export default function Contact() {
 
-
-
   const form = useRef();
+
+  // Estado para los campos del formulario y los errores
+  const [userName, setUserName] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errors, setErrors] = useState({});
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    if (!userName || !password || !email) {
-      alert('Completar el formulario');
-      return;
-  }
+      // Validación de campos
+      if (!userName || !userEmail || !message) {
+        const newErrors = {};
+        if (!userName) newErrors.userName = 'Nombre es requerido';
+        if (!userEmail) newErrors.userEmail = 'Correo electrónico es requerido';
+        if (!message) newErrors.message = 'Mensaje es requerido';
+        setErrors(newErrors);
+        return;
+      }
+    
 
     emailjs.sendForm('service_x1e3vrd', 'template_na19q4k', form.current, 'u1hxAI5tL07G9vq2X')
       .then((result) => {
           console.log(result.text);
+
           //Limpiamos el formulario
-          form.current.reset();
-      }, (error) => {
+          setUserName('');
+          setUserEmail('');
+          setMessage('');
+          
+          //Limpiamos los errores
+          setErrors({});
+          
+          setSuccessMessage('El correo se ha enviado con éxito.');
+          
+          // Limpiamos el mensaje de éxito después de unos segundos
+          setTimeout(() => setSuccessMessage(''), 5000);
+
+        }, (error) => {
           console.log(error.text);
       });
 
@@ -35,28 +58,46 @@ export default function Contact() {
 
       <form ref={form} onSubmit={sendEmail}>
 
+      {<p className="success">{successMessage}</p>}
+
         <div className="input-box">
+          <div className='input-cont'>
             <input 
               type='text' 
               placeholder='Nombre' 
               name='user_name'
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
               >
             </input>
-            <input 
-              type='email' 
-              placeholder='Correo Electronico' 
-              name='user_email'
-              >
-            </input>
+            {<span className="error">{errors.userName}</span>}
+          </div>
+
+            <div className='input-cont'>
+              <input 
+                type='email' 
+                placeholder='Correo Electronico' 
+                name='user_email'
+                value={userEmail}
+                onChange={(e) => setUserEmail(e.target.value)}
+                >
+              </input>
+              { <span className="error">{errors.userEmail}</span>}
+            </div>
         </div>
 
-        <textarea 
-          name='message' 
-          cols="24" 
-          rows="7" 
-          placeholder='Mensaje'
-          >
-        </textarea>
+        <div className='div-textarea'>
+          <textarea 
+            name='message' 
+            cols="24" 
+            rows="7" 
+            placeholder='Mensaje'
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            >
+          </textarea>
+          {<span className="error">{errors.message}</span>}
+        </div>
 
         <input type='submit' value="Enviar Mensaje" className='btn'></input>
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,15 +4,25 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
 
 export default function Footer() {
+
+    const handleScrollToTop = () => {
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth',
+        });
+    }
+
   return (
     <footer className='footer'>
         <div className="footer-text">
-            <p>2023</p>
+            <p>Miguel Jarecca Ccallo {new Date().getFullYear()}</p>
         </div>
 
-        <div className="footer-icon">
-            <a href="/"> <FontAwesomeIcon icon= {faArrowUp}>icon</FontAwesomeIcon></a>
-        </div>
+        {/* <div className="footer-icon"> */}
+            <button type="button" onClick={handleScrollToTop}>
+                <FontAwesomeIcon icon={faArrowUp} />
+            </button>
+        {/* </div> */}
     </footer>
 
     )
