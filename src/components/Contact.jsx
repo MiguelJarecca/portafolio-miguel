f0e57d18e691a0136a950c85947b221d81d1e2d0
miguel <miguel.jarecca@gmail.com>
2024-01-10 19:23:27 -0500

    se limpia el formulario luego de enviar los datos

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 
 export default function Contact() {
 
+
+
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
+    if (!userName || !password || !email) {
+      alert('Completar el formulario');
+      return;
+  }
+
     emailjs.sendForm('service_x1e3vrd', 'template_na19q4k', form.current, 'u1hxAI5tL07G9vq2X')
       .then((result) => {
           console.log(result.text);
+          //Limpiamos el formulario
+          form.current.reset();
       }, (error) => {
           console.log(error.text);
       });
+
   };
 
   return (
@@ -29,12 +39,14 @@ export default function Contact() {
             <input 
               type='text' 
               placeholder='Nombre' 
-              name='user_name'>
+              name='user_name'
+              >
             </input>
             <input 
               type='email' 
               placeholder='Correo Electronico' 
-              name='user_email'>
+              name='user_email'
+              >
             </input>
         </div>
 
@@ -42,7 +54,8 @@ export default function Contact() {
           name='message' 
           cols="24" 
           rows="7" 
-          placeholder='Mensaje'>
+          placeholder='Mensaje'
+          >
         </textarea>
 
         <input type='submit' value="Enviar Mensaje" className='btn'></input>
