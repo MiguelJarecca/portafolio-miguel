2a80a3b685c94fa738dc5223faf9f342326a7249
miguel <miguel.jarecca@gmail.com>
2025-02-15 18:30:18 -0500

    se actualiza la sección de habilidades

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,13 +4,16 @@ import {faHtml5,faCss3Alt,faSquareJs} from '@fortawesome/free-brands-svg-icons';
 import {faBootstrap,faReact} from '@fortawesome/free-brands-svg-icons';
 
 import { FaJava } from "react-icons/fa";
-import { faNodeJs } from '@fortawesome/free-brands-svg-icons';
 import { SiSpring } from "react-icons/si";
 import { SiMysql } from "react-icons/si";
 import { SiSpringsecurity } from "react-icons/si";
+import { faDocker } from '@fortawesome/free-brands-svg-icons';
 
 import { faGit, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { SiVisualstudiocode } from "react-icons/si";
+import { SiKubernetes } from "react-icons/si";
+import { SiSwagger } from "react-icons/si";
+import { SiIntellijidea } from "react-icons/si";
 
 import SkillsItem from './SkillsItem';
 
@@ -20,23 +23,28 @@ export default function Skills() {
     const arrayIcons = [
         {icono:faHtml5, clase:'f-item html-icon'},
         {icono:faCss3Alt, clase:'f-item css-icon'},
+        {icono:faReact, clase:'f-item react-icon'},
         {icono:faSquareJs, clase:'f-item js-icon'},
         {icono:faBootstrap, clase:'f-item boostrap-icon'},
-        {icono:faReact, clase:'f-item react-icon'}
     ];
 
     const backendIcons = [
         {icono:<FaJava/>, clase:'f-item java-icon'},
-        {icono:faNodeJs, clase:'f-item nodeJs-icon'},
         {icono: <SiSpring/>, clase:'f-item siSpring-icon'},
-        {icono: <SiMysql/>, clase:'f-item siMySql-icon'},
         {icono: <SiSpringsecurity/>, clase:'f-item siSpringSecurity-icon'},
+        {icono: <SiMysql/>, clase:'f-item siMySql-icon'},
     ];
 
     const othersIcons = [
+        {icono:faDocker, clase:'f-item docker-icon'},
+        {icono:<SiKubernetes/>, clase:'f-item siKuberenetes-icon'},
+        {icono:<SiSwagger/>, clase:'f-item siSwagger-icon'},
+
         {icono:faGit, clase:'f-item git-icon'},
         {icono:faGithub, clase:'f-item gitHub-icon'},
+        {icono:<SiIntellijidea/>, clase:'f-item siIntellijidea-icon'},
         {icono:<SiVisualstudiocode/>, clase:'f-item siVisualStudioCode-icon'},
+
     ]; 
 
   return (
