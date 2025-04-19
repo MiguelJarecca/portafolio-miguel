e5e7550633eab35fa8476cfd6624f751e7a87c5c
miguel <miguel.jarecca@gmail.com>
2025-04-19 11:18:52 -0500

    se empieza a dar esstilos a la sección de contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -34,63 +34,67 @@ export default function Contact() {
 
       <h2 className="heading">Contacta<span> me</span></h2>
 
-      <div className="div-what-app">
-        <a href="https://web.facebook.com/miguel.jareccaccallo.1/?locale=es_LA" 
-            target="_blank">
-            <ReactIcon/>
-        </a> 
+      <div className="contact-container">
+          <a href="https://web.facebook.com/miguel.jareccaccallo.1/?locale=es_LA" 
+              target="_blank">
+              <ReactIcon/>
+          </a> 
+
+        <form className='form' onSubmit={onSubmitUserForm}>
+
+          <div className="form-container">
+            <div className="input-box">
+                <input 
+                  type="text" 
+                  id='name' 
+                  placeholder=' '
+                  name='name'
+                  value={name}
+                  onChange={onInputUserForm}
+                />
+
+                <label htmlFor="name">Nombre:</label>
+                <span></span>
+              </div>
+
+              <div className="input-box">
+                <input 
+                  type="email" 
+                  id='email' 
+                  placeholder=' '
+                  name='email'
+                  value={email} 
+                  onChange={onInputUserForm}
+                  />
+
+                <label htmlFor="email">Correo Electrónico:</label>
+                <span></span>
+              </div>        
+
+            <div className="input-box">
+              <input 
+                type="text"
+                id='textarea'
+                placeholder=' '
+                name='textarea'
+                value={textarea}
+                onChange={onInputUserForm}
+
+                />
+              <label htmlFor="email">Mensaje:</label>
+              <span></span>
+            </div>
+
+            <button 
+              type='submit'
+              
+              >
+                Enviar
+            </button>
+          </div>
+            
+        </form>
       </div>
-
-    <form onSubmit={onSubmitUserForm}>
-      <div className="input-box">
-        <div className="input-group">
-          <input 
-            type="text" 
-            id='name' 
-            placeholder=' '
-            name='name'
-            value={name}
-            onChange={onInputUserForm}
-          />
-
-          <label htmlFor="name">Nombre:</label>
-          <span></span>
-        </div>
-
-        <div className="input-group">
-          <input 
-            type="email" 
-            id='email' 
-            placeholder=' '
-            name='email'
-            value={email} 
-            onChange={onInputUserForm}
-            />
-
-          <label htmlFor="email">Correo Electrónico:</label>
-          <span></span>
-        </div>        
-      </div>
-      <div className="input-textarea">
-        <input 
-          type="text"
-          id='textarea'
-          placeholder=' '
-          name='textarea'
-          value={textarea}
-          onChange={onInputUserForm}
-
-           />
-      </div>
-
-      <button 
-        type='submit'
-        
-        >
-          Enviar
-        </button>
-    </form>
-   
 
     </section>
   )
